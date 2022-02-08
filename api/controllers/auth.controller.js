472e133b7923b8f7d597c9d421237f35e9951c6f
Yanira Alemán <yanira.ar@gmail.com>
2022-02-08 21:52:50 +0000

    AUTH - signup function created
    MODELS
    - User model created
    - Address model created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,3 +2,20 @@ const UserModel = require ('../models/user.model')
 
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+
+async function signup (req, res) {
+  try {
+    const hash = bcrypt.hashSync(req.body.password, 10)
+    req.body.password = hash
+
+    const user = await UserModel.create(req.body)
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error creating user')
+  }
+}
+
+module.exports = {
+  signup
+}
---
+++ api/models/address.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const addressSchema = new mongoose.Schema({
+  street: {
+    type: String,
+    required: [true, 'Street is required']
+  },
+  number: {
+    type: Number,
+  },
+  flat: {
+    type: Number,
+  },
+  door: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  postalCode: {
+    type: Number,
+  }
+})
+const AddressModel = mongoose.model('address', addressSchema)
+
+module.exports = AddressModel
