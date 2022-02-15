4bcb070e36746fa2f977c1c219844273d6a90441
Juan Antonio León <jalojr@hotmail.com>
2022-02-15 12:36:26 +0000

    test.controller: took out base64 in getOneTest and simplified (a little bit) submitTest

--- api/controllers/test.controller.js
+++ api/controllers/test.controller.js
@@ -1,5 +1,3 @@
-const imageToBase64 = require('image-to-base64')
-
 const TestModel = require('../models/test.model')
 
 async function getAllTests(req, res) {
@@ -13,29 +11,17 @@ async function getAllTests(req, res) {
 
 async function getOneTest(req, res) {
   try {
-    let list = []
+    const list = []
     await TestModel.findById(req.params.id, { correct: 0, answered: 0, percentage: 0, __v: 0 })
       .populate('questions')
       .then(test => {
-        async function getImage(element) {
-          if(element.picture !== undefined) {
-            const img = await imageToBase64(`./public/images/${element.picture}`)
-            const question = { question: element.text, image: img, options: element.options }
-            list.push(question)
-          }
-          else {
-            const question = { question: element.text, options: element.options }
-            list.push(question)
-          }
-        }
         test.questions.forEach(element => {
-          getImage(element)
+          console.log(element)
+          const question = { question: element.text, image: element.picture, options: element.options }
+          list.push(question)
         })
       })
-
-    Promise.all(list).then(result => {
-      res.status(200).json(result)
-    })
+      res.status(200).json(list)
   } catch (error) {
     res.status(500).send(`Request error: ${error}`)
   }
@@ -75,7 +61,7 @@ async function submitTest(req, res) {
         const answers = test.questions.map(element => { //correct answers
           return element.answer
         })
-        const text = test.questions.map(element => { //question text
+        const text = test.questions.map(element => {    //question text
           return element.text
         })
         const ansText = test.questions.map(element => { //answer text
@@ -87,53 +73,55 @@ async function submitTest(req, res) {
         const topics = test.questions.map(element => {
           return element.topic
         })
-        const submit = Object.values(req.body)  //Submitted answers
+        const submit = Object.values(req.body)      //Submitted answers
         test.answered = submit.length
         const results = []
+        const arr = user.studentData.statistics
         for (let i = 0; i < submit.length; i++) {
-          if (submit[i] === answers[i]) {                                                                               //Resp. correcta
+          if (submit[i] === answers[i]) {           //Resp. correcta
             test.correct++
             results.push({ question: `Question ${i + 1} - '${text[i]}'`, answer: `Correct! Your answer was ${answers[i]} - '${ansText[i]}'.`, correct: true })
-            const index = user.studentData.statistics.findIndex(object => { //Buscar dentro de las estadísticas si se han respondido preguntas de este topic previamente
+            const index = arr.findIndex(object => { //Buscar dentro de las estadísticas si se han respondido preguntas de este topic previamente
               if (object.topic.toString() === topics[i].toString()) { return true }
             })
             if (index === -1) {
-              user.studentData.statistics.push({ topic: topics[i] })
-              user.studentData.statistics[user.studentData.statistics.length-1].correct++
-              user.studentData.statistics[user.studentData.statistics.length-1].answered++
-              user.studentData.statistics[user.studentData.statistics.length-1].percentage = (user.studentData.statistics[user.studentData.statistics.length-1].correct / user.studentData.statistics[user.studentData.statistics.length-1].answered) * 100
+              arr.push({ topic: topics[i] })
+              arr[arr.length-1].correct++
+              arr[arr.length-1].answered++
+              arr[arr.length-1].percentage = (arr[arr.length-1].correct / arr[arr.length-1].answered) * 100
             } else {
-              user.studentData.statistics[index].correct++
-              user.studentData.statistics[index].answered++
-              user.studentData.statistics[index].percentage = (user.studentData.statistics[index].correct / user.studentData.statistics[index].answered) * 100
+              arr[index].correct++
+              arr[index].answered++
+              arr[index].percentage = (arr[index].correct / arr[index].answered) * 100
             }
-          } else if (submit[i] === '') {                                                                                  //Resp. en blanco
+          } else if (submit[i] === '') {            //Resp. en blanco
             test.answered--
             results.push({ question: `Question ${i + 1} - '${text[i]}'`, answer: `The correct answer was ${answers[i]} - '${ansText[i]}'.`, correct: false })
-          } else {                                                                                                        //Resp. errónea
+          } else {                                  //Resp. errónea
             results.push({ question: `Question ${i + 1} - '${text[i]}'`, answer: `The correct answer was ${answers[i]} - '${ansText[i]}'.`, correct: false })
-            const index = user.studentData.statistics.findIndex(object => { //Buscar dentro de las estadísticas si se han respondido preguntas de este topic previamente
+            const index = arr.findIndex(object => { //Buscar dentro de las estadísticas si se han respondido preguntas de este topic previamente
               if (object.topic.toString() === topics[i].toString()) { return true }
             })
             if (index === -1) {
-              user.studentData.statistics.push({ topic: topics[i] })
-              user.studentData.statistics[user.studentData.statistics.length-1].answered++
-              user.studentData.statistics[user.studentData.statistics.length-1].percentage = (user.studentData.statistics[user.studentData.statistics.length-1].correct / user.studentData.statistics[user.studentData.statistics.length-1].answered) * 100
+              arr.push({ topic: topics[i] })
+              arr[arr.length-1].answered++
+              arr[arr.length-1].percentage = (arr[arr.length-1].correct / arr[arr.length-1].answered) * 100
             } else {
-              user.studentData.statistics[index].answered++
-              user.studentData.statistics[index].percentage = (user.studentData.statistics[index].correct / user.studentData.statistics[index].answered) * 100
+              arr[index].answered++
+              arr[index].percentage = (arr[index].correct / arr[index].answered) * 100
             }
           }
         }
-        const index = user.studentData.testsDone.findIndex(object => { //Buscar dentro de los tests realizados por el alumno si ya había hecho este test antes
+        const arrTests = user.studentData.testsDone
+        const index = arrTests.findIndex(object => { //Buscar dentro de los tests realizados por el alumno si ya había hecho este test antes
           if (object.id.toString() === req.params.id) { return true }
         })
         if (index === -1) {
-          user.studentData.testsDone.push({ id: req.params.id }) //Si no se había hecho el test antes, añadirlo a los test realizados
-          user.studentData.testsDone[user.studentData.testsDone.length - 1].maxScore = test.correct //establecemos la puntación obtenida como maxScore
-        } else {  //en caso de que el test ya se haya realizado previamente
-          test.correct > user.studentData.testsDone[index].maxScore ? user.studentData.testsDone[index].maxScore = test.correct : user //modificar puntuación máxima si se ha superado
-          user.studentData.testsDone[index].tries++// aumentar número de intentos
+          arrTests.push({ id: req.params.id })                //Si no se había hecho el test antes, añadirlo a los test realizados
+          arrTests[arrTests.length - 1].maxScore = test.correct //establecemos la puntación obtenida como maxScore
+        } else {                                                //en caso de que el test ya se haya realizado previamente
+          test.correct > arrTests[index].maxScore ? arrTests[index].maxScore = test.correct : user //modificar puntuación máxima si se ha superado
+          arrTests[index].tries++                               // aumentar número de intentos
         }
         user.save()
         test.percentage = (test.correct / answers.length) * 100
